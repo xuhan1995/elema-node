689054339c64ea05e912746d710c7bc847de6dc9
hanxu <2017111600@bupt.edu.cn>
2018-09-26 23:40:10 +0800

    feature:set class citiesHandler to deal cities requset

---
+++ controller/v1/cities.js
@@ -0,0 +1,43 @@
+'use strict';
+
+import Cities from '../../models/v1/cities'
+import pinyin from "pinyin"
+
+class CitiesHandler {
+	constructor() {
+		this.getCities = this.getCities.bind(this)
+	}
+
+	async getCities(req, res) {
+		const type = req.query.type
+		let citiesInfo
+		try {
+			switch (type) {
+				case 'guess':
+					console.log('coming soon...')
+					break
+				case 'hot':
+					console.log('coming soon...')
+					break
+				case 'group':
+					citiesInfo = await Cities.citiesGroup()
+					break
+				default:
+					res.json({
+						name: 'ERROR_QUERY_TYPE',
+						message: '参数错误',
+					})
+					return
+			}
+			res.send(citiesInfo)
+		} catch (error) {
+			res.send({
+				name: 'ERROR_DATA',
+				message: '获取数据失败',
+			})
+			console.error(error)
+		}
+	}
+}
+
+export default new CitiesHandler
