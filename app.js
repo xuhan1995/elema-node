137715de7926c0db3350c1b859738bbda55971b3
hanxu <2017111600@bupt.edu.cn>
2018-10-11 18:16:40 +0800

    feature: add comment

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ const app = express()
 
 //set res.header
 app.all('*', (req, res, next) => {
-	res.header("Access-Control-Allow-Origin", "*")
-	res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With")
+	res.header("Access-Control-Allow-Origin", "*")  //允许的域设置全部，解决跨域
+	res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With") //允许的header
 	res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS")
   res.header("Access-Control-Allow-Credentials", true); //可以带cookies
 	res.header("X-Powered-By", '3.2.1')
