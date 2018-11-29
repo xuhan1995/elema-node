6de4fcaccbbdcee28aa37860483ddcec2da68887
hanxu <2017111600@bupt.edu.cn>
2018-11-30 01:02:49 +0800

    feature: add food

--- routes/shopping.js
+++ routes/shopping.js
@@ -15,5 +15,6 @@ router.get('/v1/restaurants/delivery_modes', category.getDeliveries)
 router.get('/v1/restaurants/activity_attributes', category.getActivities)
 router.post('/addshop', check.checkAdmin, shop.addShop)
 router.post('/addcategory', check.checkAdmin, food.addCategory)
+router.post('/addfood', check.checkAdmin, food.addFood)
 
 export default router
