0d05a32a095fe2eaa7584d2029b55b8d5fa159ee
hanxu <2017111600@bupt.edu.cn>
2018-12-13 00:31:27 +0800

    fix: compatible with axios

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express()
 
 // set res.header
 app.all('*', (req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*")  //允许的域设置全部，解决跨域
+  res.header("Access-Control-Allow-Origin", req.headers.Origin || req.headers.origin)
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With, Cache-Control") //允许的header
   res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS")
   res.header("Access-Control-Allow-Credentials", true); //可以带cookies
