77081c38a9f77615d17aa8e7cd4d69ee077c180b
hanxu <2017111600@bupt.edu.cn>
2018-12-14 00:46:35 +0800

    chore: remove useless request method

--- app.js
+++ app.js
@@ -17,11 +17,7 @@ app.all('*', (req, res, next) => {
   res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS")
   res.header("Access-Control-Allow-Credentials", true); //可以带cookies
 	res.header("X-Powered-By", '3.2.1')
-	if (req.method == 'OPTIONS') {
-	  	res.sendStatus(200);
-	} else {
-	    next();
-	}
+	next()
 })
 
 app.use(cookieParser())
